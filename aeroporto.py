ce779acdca088b7e3548d1322713f2b231b52af7
Mateus Chou <choumateus@gmail.com>
2020-03-02 00:00:15 -0300

    desenvolvendo

--- aeroporto.py
+++ aeroporto.py
@@ -1,27 +1,37 @@
 import random
 class fila:
+
     def __init__(self):
         self.fila = [] #construtor
+
     def vazio(self):
         return len(self.fila) == 0
+
     def insere(self, i):
         self.fila.append(i)
+
     def tira(self):
         if self.vazio():
             raise Exception ("fila vazia")
         return self.fila.pop(0)
+
     def primeiro(self):
         if self.vazio():
             raise Exception ("fila vazia")
         return self.fila[0]
+
     def __getitem__(self, i):
         return self.fila[i]
+
     def __setitem__(self, i, j):
         self.fila[i] = j
-    def mostre(self):
-        print(self.fila)
+
+    def __str__(self):
+        if len(self.fila) > 0:
+            return str(self.fila[0])
+        return str(None)
 class aviao:
-    def __init__(self, aero= "AAA", comp="AA", num = 000, temp = 0, situ = "P",emerg = "N",pouso_decolagem = 2):
+    def __init__(self, aero= "AAA", comp="AA", num = 000, temp = 0, situ = "P",emerg = "N",pouso_decolagem = 2, existencia = 1):
         self.aeroporto = aero
         self.companhia = comp
         self.num = num
@@ -29,36 +39,32 @@ class aviao:
         self.situ =situ  # P para pousando, D para decolando
         self.pouso_decolagem = pouso_decolagem
         self.emerg = emerg
+        self.existencia = existencia
     def emergencia(self):
         return self.emerg == "S"
     def passou_tempo(self):
-        if self.pouso_decolagem > 0:
-            self.pouso_decolagem-=1
+        if self.pouso_decolagem >= 0:
+            #print("ainda nao")
             return False
-        else:
+        elif self.pouso_decolagem == -1:
+            #print("deu 0")
             return True
     def esvazia(self):
-        self.existe = 0
-    def existe(self):
-        return self.existe == 1
+        self.existencia = 0
     def __str__(self):
-        if self.existe == 1:
-            return "companhia : " + str(self.companhia) + " numero: " + str(self.num) + " aeroporto: " + str(self.aeroporto)
-        else :
+        if self.existencia == 1:
+            return "companhia : " + str(self.companhia) + " numero: " + str(self.num) + " aeroporto: " + str(self.aeroporto) + " emergencia: " + str(self.emerg) + str(self.pouso_decolagem) + " " + str(self.existencia)
+        elif self.existencia == 0 :
             return "vazia"
+    def reduz_tempo(self):
+        self.pouso_decolagem-=1
+    def existe(self):
+        return self.existencia == 1
 class pistas:
     def __init__(self):
-        self.pistas= [None,None]
-    def vazio(self):
-        if  self.pistas[0].existe():
-            return False
-        else:
-            return True
-    def append(self, a):
-        for i in range(len(self.pistas)):
-            if self.pistas[i]==None:
-                self.pistas[i] = a
-                break
+        self.pistas= [aviao(existencia=0),aviao(existencia=0),aviao(existencia=0)]
+    def append(self, a , b):
+        self.pistas[b] = a
     def __len__(self):
         return len(self.pistas)
     def __getitem__(self, i):
@@ -68,6 +74,7 @@ class pistas:
     def __str__(self):
         return "pista 1: " + str(self.pistas[0]) +  "\n pista 2: " + str(self.pistas[1]) + "\n pista 3: " + str(pistas[2])
 pistas = pistas()
+fila = fila()
 testes = random.randint(10, 30)
 nums = "0123456789"
 situs = "PD"
@@ -75,10 +82,13 @@ letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for i in range(testes):
     k = random.randint(0,3) #quantidade de avioes que vao entrar
     for m in range (len(pistas)):
-        if pistas[m] != None:
+        if pistas[m].existe():
+            if not  pistas[m].passou_tempo():
+                pistas[m].reduz_tempo()
             if pistas[m].passou_tempo():
-                pistas[m].esvazia()
+                pistas[m].existencia = 0
     for j in range (k):
+        novo = 0
         aero = letras[random.randint(0,25)] + letras[random.randint(0,25)] + letras[random.randint(0,25)]
         comp =  letras[random.randint(0,25)] + letras[random.randint(0,25)]
         num = int(nums[(random.randint(0,9))]+nums[(random.randint(0,9))]+nums[(random.randint(0,9))])
@@ -88,8 +98,15 @@ for i in range(testes):
             A= aviao(aero,comp,num,temp,situ, "S")
         else:
             A = aviao(aero,comp,num,temp,situ)
-        if pistas.vazio():
-            pistas.append(aviao)
+        for i in range (len(pistas)):
+            if pistas[i].existencia == 0:
+                pistas.append(A, i)
+                novo=1
+                break
+        if novo == 0 :
+            fila.insere(A)
+    print(pistas)
+    print(fila)
 
 
 
