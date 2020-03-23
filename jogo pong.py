fafb58aa097ef3f67ec1d2c55273a76d0b269ddd
Mateus Chou <choumateus@gmail.com>
2020-03-23 01:44:03 -0300

    pong completo

--- jogo pong.py
+++ jogo pong.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 #tela
 tela = turtle.Screen()
 tela.title("Pong")
@@ -31,8 +32,8 @@ bola.shape("square")
 bola.color("white")
 bola.penup()
 bola.goto(0,0) #coordenadas
-bola.dx = 0.1 #direcoes e velocidade
-bola.dy = 0.1
+bola.dx = 0.2 #direcoes e velocidade
+bola.dy = 0.2
 
 #*********  funcoes *****************
 
@@ -56,6 +57,20 @@ def barra2_pra_baixo():
     y -= 20 #coordenadas de y mais 20
     barra2.sety(y) #jogando a barra para cima
 
+
+
+#criando o placar
+caneta = turtle.Turtle()
+caneta.speed(0)
+caneta.color("white")
+caneta.penup()
+caneta.hideturtle()
+caneta.goto(0,260)
+caneta.write("Jogador1: 0  Jogador2: 0", align = "center", font=("Courier", 24, "normal"))
+pontos_jogador1= 0
+pontos_jogador2=0
+
+
 #*********  conexao com o teclado   ***********
 tela.listen()
 tela.onkeypress(barra1_pra_cima, "w")
@@ -74,23 +89,36 @@ while True:
 
     #quando a bola atinge os topos
     if bola.ycor() > 290:
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.sety(290)
         bola.dy *= -1
     if bola.ycor() < -290:
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.sety(-290)
         bola.dy *= -1
     if bola.xcor() > 390:
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.goto(0,0)
         bola.dx *= -1
+        pontos_jogador1+=1
+        caneta.clear()
+        caneta.write("Jogador1: "+ str(pontos_jogador1)+"  Jogador2: "+ str(pontos_jogador2), align="center", font=("Courier", 24, "normal"))
     if bola.xcor() < -390:
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.goto(0,0)
         bola.dx *= -1
+        pontos_jogador2+=1
+        caneta.clear()
+        caneta.write("Jogador1: " + str(pontos_jogador1) + "  Jogador2: " + str(pontos_jogador2), align="center",
+                     font=("Courier", 24, "normal"))
 
     #quando a bola atinge a barra
     if (bola.xcor() > 340 and bola.xcor() < 350) and (bola.ycor() < barra2.ycor() + 50 and bola.ycor() > barra2.ycor() - 50 ):
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.setx(340)
         bola.dx *= -1
     if (bola.xcor() < -340 and bola.xcor() > -350) and (bola.ycor() < barra1.ycor() + 50 and bola.ycor() > barra1.ycor() - 50):
+        winsound.PlaySound("test.wav", winsound.SND_ASYNC)
         bola.setx(-340)
         bola.dx *= -1
 
