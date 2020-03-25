4a41b1a2dc5ed4cba9d66273b1907074fc338ce1
Mateus Chou <choumateus@gmail.com>
2020-03-24 21:30:00 -0300

    monte carlo atualizado

--- metodos de montecarlo.py
+++ metodos de montecarlo.py
@@ -1,49 +1,67 @@
 from random import randrange
 import math as mt
-#primeiro vamos estimar um valor muito proximo do valor da intergral que queremos calcular
-#para isso vamos simular varias vezes monte carlo e achar a media de todas as simulacoes
-b=0.11221352      #NUSP
+#funcao desvio padrao para usarmos a propriedade do erro=DV/raiz de N
+def desvio_padrao(x,xm):
+    var=0
+    for i in range(len(x)):
+       var+=(x[i]-xm)**2/(len(x)-1)
+    return var**(1/2)
+
+#vamos montar a funcao dada do enunciado
+b=0.11221352     #NUSP
 a=0.501925028  #RG
-MC = [] #lista com os resultados das simulacoes
-N = 200 #vamos simular 100 vezes
-n = 10000 #vamos ter 10000 numeros aleatorios entre 0 e 1 para x
-integral = 0
 def f(x):
     return mt.e**(-a*x)*mt.cos(b*x)
-for i in range(N):
-    soma = 0
-    for j in range(n):
-        x=randrange(0,10000000)/10000000 #numero aleatorio entre 0 e 1
-        soma+=f(x)
-    MC.append(soma/n)#uma integral encontrada
-for j in MC:
-    integral+=j/N #media das integrais
 
 #Metodo Crude Monte Carlo
 #queremos a media de uma amostra de n x no qual ela se aproxima do valor da integral
 n1=1
-integral_estimada1=0
+soma1=0
 erro1 = 5 #qualquer numero maior que 0,01
+xl=[]
+integral_estimada1 = 0
 while erro1 >= 0.01:
-    integral_estimada1 = 0
-    for i in range(n):
-        x=randrange(0,10000000)/10000000
-        integral_estimada1 += f(x)/n
-    erro1 = abs(integral_estimada1-integral)/integral
-    n+=1
+    x=randrange(0,10000000)/10000000
+    soma1 += f(x)
+    xl.append(f(x))
+    integral_estimada1 = soma1/n1
+    if n1<5:
+        n1+=1
+    else:
+        dv=desvio_padrao(xl,integral_estimada1)
+        erro1 = dv/(n1)**(1/2)
+        n1+=1
 print("valor estimado:", integral_estimada1)
 print("tamanho da amostra:", n1)
-print("erro do Crude= ", erro1)
+print("erro do Crude pela variancia= ", erro1)
+
 
 #Metodo hit-miss
 #pegamos coordenadas aleatorias,caso ela esteja "dentro da curva"contamos
-#primeiro precisamos estimar o ponto maximo, para obtermos o "retangulo" dos numeros aleatorios
-maxy = 0
-for i in range(1000):
-    x = i/1000
-    print(f(x))
-    if f(x) > maxy:
-        maxy = f(x)
-        print(maxy)
-x , y = randrange(0,1) , randrange(0,1)
-print(3)
+#sabemos atraves de uma analise de grafico que a funcao f entre 0 e 1 possui um valor maximo de um
+#definimos a funcao desvio padrao para uma bernoulli, ja que se trata dessa distribuicao
+def desvio_padrao_bernoulli(p):
+    return (p*(1-p))**(1/2)
+n2 = 1
+integral_estimada2 = 0
+erro2 = 5
+soma = 0
+while erro2 >= 0.01:
+    x , y = randrange(0,10000000)/10000000 , randrange(0,10000000)/10000000 #numeros entre 0 e 1
+    if f(x) > y:
+        soma+=1
+    integral_estimada2 = soma/n2
+    if n2>10:
+        erro2 = desvio_padrao_bernoulli(integral_estimada2)/(n2)**(1/2)
+    #print(integral_estimada2)
+    n2+=1
+
+print("valor estimado:", integral_estimada2)
+print("tamanho da amostra:", n2-1)
+print("erro do hit-miss= ", erro2)
+
+
+
+
+
+
